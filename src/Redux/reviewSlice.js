9c11d8f98a59195d7cb32c0815148b0d529e23c6
umesh2003 <umeshhiwarale3@gmail.com>
2024-04-16 20:19:45 +0530

    Added a new page

--- src/Redux/reviewSlice.js
+++ src/Redux/reviewSlice.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { createSlice } from "@reduxjs/toolkit";
 import { ADD_REVIEW, GET_REVIEWS } from "../Constants/urls";
 import { displayToast } from "./toastSlice";
